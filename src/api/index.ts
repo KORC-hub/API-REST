5e7a383de5e84ad124ae23effe91433aeb5ea664
Kevin Rogers <rogerscardonakevin@gmail.com>
2025-05-07 16:18:39 -0500

    rename import of routes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
-import items from "./routes/routers.ts";
+import routes from "./routes/routers.ts";
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use("/elements", items);
+
+app.use("/api", routes);
 
 app.listen(3001, () => {
   console.log("Server is running on http://localhost:3001/");
