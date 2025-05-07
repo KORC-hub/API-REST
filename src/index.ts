e00e26a03d2512aa5ed014330ac559ba163dcbfd
Kevin Rogers <rogerscardonakevin@gmail.com>
2025-05-07 00:02:13 -0500

    add index file

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+import items from "./items/routers.ts";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use("/elements", items);
+
+app.listen(3001, () => {
+  console.log("Server is running on http://localhost:3001/");
+});
