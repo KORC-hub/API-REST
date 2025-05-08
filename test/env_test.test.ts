c84d6fbe6b7c631b0e8e94387622dd39160da341
Kevin Rogers <rogerscardonakevin@gmail.com>
2025-05-07 22:34:57 -0500

    tests with chai library

--- test/env_test.test.ts
+++ test/env_test.test.ts
@@ -1,36 +1,35 @@
-import assert from "node:assert";
+import { assert, expect } from "chai";
+import { describe, it } from "mocha";
 import { tiMonth, fuelEnergySelector } from "../src/calculations/environment.ts";
 
 describe("tiMonth test", () => {
   it("case 1", () => {
-    assert.strictEqual(tiMonth(1.4), 5);
+    expect(tiMonth(1.4)).to.equal(5);
   });
 });
 
 describe("fuelEnergySelector test collection", () => {
-  it('Diesel case"', () => {
+  it("Diesel case", () => {
     const expectedResult = {
       fuel_price: 11795,
       fuel_energy: 40.7,
       emision_factor: 74.01,
     };
-    assert.deepStrictEqual(fuelEnergySelector("diesel"), expectedResult);
-    assert.deepStrictEqual(fuelEnergySelector("Diesel"), expectedResult);
+    expect(fuelEnergySelector("diesel")).to.deep.equal(expectedResult);
+    expect(fuelEnergySelector("Diesel")).to.deep.equal(expectedResult);
   });
+
   it("Gasoline case", () => {
     const expectedResult = {
       fuel_price: 16700,
       fuel_energy: 35.58,
       emision_factor: 69.25,
     };
-    assert.notEqual(fuelEnergySelector("gasoline"), expectedResult);
-    assert.notEqual(fuelEnergySelector("Gasoline"), expectedResult);
+    expect(fuelEnergySelector("gasoline")).to.deep.equal(expectedResult);
+    expect(fuelEnergySelector("Gasoline")).to.deep.equal(expectedResult);
   });
-  it("not return empty object", () => {
-    assert.notDeepEqual(fuelEnergySelector("Diesel"), {});
-    assert.notDeepEqual(fuelEnergySelector("gasoline"), {});
-  });
+
   it("Prueba con otro combustible", () => {
-    assert.strictEqual(fuelEnergySelector("GLP"), "Tipo de combustible no valido");
+    expect(fuelEnergySelector("GLP")).to.equal("Tipo de combustible no valido");
   });
 });
