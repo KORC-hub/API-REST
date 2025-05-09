686f1891025eb13a2380026dc525f751be2debcc
Kevin Rogers <rogerscardonakevin@gmail.com>
2025-05-09 14:47:44 -0500

    other tests have been added

--- test/env_test.test.ts
+++ test/env_test.test.ts
@@ -1,6 +1,14 @@
 import { assert, expect } from "chai";
 import { describe, it } from "mocha";
-import { tiMonth, fuelEnergySelector } from "../src/calculations/environment.ts";
+import {
+  tiMonth,
+  fuelEnergySelector,
+  electricalConsumption,
+  costElectricalKM,
+  combustionConsumption,
+  fuelConsumption,
+} from "../src/calculations/environment.ts";
+import type { FuelInfo } from "../src/types/environmentTypes.ts";
 
 describe("tiMonth test", () => {
   it("case 1", () => {
@@ -30,6 +38,47 @@ describe("fuelEnergySelector test collection", () => {
   });
 
   it("Prueba con otro combustible", () => {
-    expect(fuelEnergySelector("GLP")).to.equal("Tipo de combustible no valido");
+    expect(() => fuelEnergySelector("GLP")).to.throw("Tipo de combustible no valido");
+  });
+});
+
+describe("electricalConsumption test collection", () => {
+  it("Result test", () => {
+    assert.strictEqual(electricalConsumption(81.14, 200), 0.4507777777777778);
+  });
+});
+
+describe("costElectricalKm test collection", () => {
+  it("Result test", () => {
+    const electricalConsumptionResult = electricalConsumption(81.14, 200);
+    assert.strictEqual(costElectricalKM(electricalConsumptionResult, 238.25), 107.39780555555555);
+  });
+});
+
+describe("combustionConsumption test collection", () => {
+  it("Result test", () => {
+    const electricalConsumptionResult = electricalConsumption(81.14, 200);
+    assert.strictEqual(combustionConsumption(electricalConsumptionResult), 1.6695473251028805);
+  });
+});
+
+describe("fuelConsumption test collection", () => {
+  it("Case fuel energy gasoline", () => {
+    const fuelEnergySelectorResult: FuelInfo = fuelEnergySelector("gasoline");
+    const electricalConsumptionResult = electricalConsumption(81.14, 200);
+    const combustionConsumptionResult = combustionConsumption(electricalConsumptionResult);
+    assert.strictEqual(
+      fuelConsumption(combustionConsumptionResult, fuelEnergySelectorResult["fuel_energy"]),
+      0.04692375843459473
+    );
+  });
+  it("Case fuel energy diesel", () => {
+    const fuelEnergySelectorResult: FuelInfo = fuelEnergySelector("diesel");
+    const electricalConsumptionResult = electricalConsumption(81.14, 200);
+    const combustionConsumptionResult = combustionConsumption(electricalConsumptionResult);
+    assert.strictEqual(
+      fuelConsumption(combustionConsumptionResult, fuelEnergySelectorResult["fuel_energy"]),
+      0.04102081879859657
+    );
   });
 });
